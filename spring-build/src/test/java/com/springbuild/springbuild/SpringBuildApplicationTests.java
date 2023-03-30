2f0f17172f00801f39a35a70afde1ecb32d61dcd
DavidLunaAd <davidlunaad@gmail.com>
2023-03-30 17:59:31 +0200

    spring heroku app config

---
+++ spring-build/src/test/java/com/springbuild/springbuild/SpringBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbuild.springbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
